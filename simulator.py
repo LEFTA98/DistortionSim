ffaa1248775b4385f63818acccf17dd23a23da9e
LEFTA98 <sqshyzerg353@gmail.com>
2020-08-26 10:54:08 -0400

    added odd-even-unreachable decomp

--- simulator.py
+++ simulator.py
@@ -29,7 +29,7 @@ class Simulator:
         self.history = {'id': [], 'size': [], 'valuation':[], 'algo': [], 'distortion': []}
 
 
-    def rankify_to_graph(self, G, agent_cap=None):
+    def rankify_graph(self, G, agent_cap=None):
         """Augments a weighted bipartite graph with nodes 1 through n with ranks. For example, if agent 1 values good n-1 as their second-highest
         valued good, assign a rank of 2 to the edge (1, n-1).
 
@@ -46,25 +46,65 @@ class Simulator:
             agent_cap = len(G.nodes)//2
 
         for i in range(1, agent_cap+1):
-            adj_list = [(key, G.adj[key][i]['weight']) for key in G.adj[i].keys()]
+            adj_list = [(key, G.adj[key][i]['weight']) for key in G.adj[i].keys() if G.adj[i][key] != {}]
             adj_list.sort(key = lambda tup: tup[1], reverse = True) #sort by second value in the tuple
 
             for j in range(len(adj_list)):
-                G[i][tup[j][0]]['rank'] = j+1
+                G[i][adj_list[j][0]]['rank'] = j+1
 
         return G
 
 
-    def even_odd_unreachable_decomposition(self, G, agent_cap=None):
+    def bfs_even_odd(self,H,G,v):
+        """Helper function that runs breadth-first search on a graph H, with source node v, augmenting every node with whether it is an even or odd distance away
+        from the source node in graph G, of which H is a subset.
+
+        Args:
+            H (nx.Graph): undirected subgraph of G.
+            G (nx.Graph): undirected graph.
+            v (Object): the name of a source node that is a part of H.
+
+        Returns:
+            NoneType
+        """
+        nx.set_node_attributes(G, {v: 'E'}, 'decomp')
+        queue = list(H.adj[v].keys())
+        discovered = set().union(set('v'), H.adj[v].keys())
+        for item in queue:
+            nx.set_node_attributes(G, {item: 'O'}, 'decomp')
+
+        while len(queue) != 0:
+            curr = queue.pop(0)
+            curr_parity = "E" if G.nodes[curr]['decomp'] == "O" else "O"
+            for key in H.adj[curr].keys():
+                if key not in discovered:
+                    discovered.add(key)
+                    queue.append(key)
+                    nx.set_node_attributes(G, {key: curr_parity}, 'decomp')
+                        
+
+    def even_odd_unreachable_decomposition(self, G):
         """Decomposes a bipartite graph into even, odd, and unreachable vertices, based on the following decomposition: 
         https://en.wikipedia.org/wiki/Dulmage%E2%80%93Mendelsohn_decomposition
 
         Args:
             G (nx.Graph): Bipartite Graph with nodes named 1 through n. Agents are assumed to be nodes 1 through i for some i <= n.
-            agent_cap (int): The numerical label of the last agent; that is, if agents are enumerated by nodes 1 through i, then agent_cap is equal to i. Defaults
-            to len(G.nodes)//2 if not given.
-        """
-        if agent_cap is None:
-            agent_cap = len(G.nodes)//2
 
-        S = nx.maximal_matching(G)
+        Returns:
+            NoneType
+        """
+        S = [item[0] for item in nx.maximal_matching(G)] + [item[1] for item in nx.maximal_matching(G)]
+        connected_components = nx.algorithms.components.connected_components(G)
+        connected_subgraphs = [G.subgraph(c).copy() for c in connected_components]
+
+        for component in connected_subgraphs:
+            if set().union(set(S), set(component.nodes)) == set(S):
+                for node in component.nodes:
+                    nx.set_node_attributes(G, {node: 'U'}, 'decomp')
+
+            #only reach here if there's at least one node in component not in max_matching
+            else:
+                T = list(S.difference(component.nodes))
+                v = T[0]
+                self.bfs_even_odd(component,G,v)
+
