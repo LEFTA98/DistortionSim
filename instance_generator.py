fixed bug with unit-range normalization, fixed data

instance_generator.py
@@ -124,7 +124,7 @@ class InstanceGenerator:
         """
         min_array = np.min(M, axis=1)
         max_array = np.max(M, axis=1)
-        return (M.T-min_array).T/(max_array-min_array) #hacky way to do row subtraction
+        return ((M.T-min_array)/(max_array-min_array)).T #hacky way to do row subtraction
 
 
     def normalize_unit_sum(self, M):

simulator.py
@@ -142,9 +142,9 @@ if __name__=='__main__':
 
         for j in range(20): # adjust number of trials per n here
 
-            G = instantiator.generate_unit_sum_abdulkadiroglu(1, n) #adjust the valuation generation method here
+            G = instantiator.generate_unit_range_abdulkadiroglu(1,n) #adjust the valuation generation method here
             val_index = instantiator.index-1
-            val_type = 'unit_sum_abdulkadiroglu_1'
+            val_type = 'unit_range_abdulkadiroglu_1'
 
             sim.serial_dictatorship_experiment(val_index,val_type,G)
             sim.partial_max_matching_experiment(val_index,val_type,G,np.floor(np.log(n)))
@@ -153,7 +153,7 @@ if __name__=='__main__':
 
     
     df = pd.DataFrame(sim.history)
-    df.to_csv("C:/Users/sqshy/Desktop/University/Fifth Year/research/DistortionSim/data/unit_sum_abdulkadiroglu_1.csv") #adjust path name here
+    df.to_csv("C:/Users/sqshy/Desktop/University/Fifth Year/research/DistortionSim/data/unit_range_abdulkadiroglu_1.csv") #adjust path name here
 
     df = pd.Series(sim.instance_generator.history)
-    df.to_csv("C:/Users/sqshy/Desktop/University/Fifth Year/research/DistortionSim/data/unit_sum_abdulkadiroglu_1_instances.csv") #adjust instance data path name here
+    df.to_csv("C:/Users/sqshy/Desktop/University/Fifth Year/research/DistortionSim/data/unit_range_abdulkadiroglu_1_instances.csv") #adjust instance data path name here